45faf4a1608b859b9a5bd33bd0ba892ef7b87b9f
Tomasz Miśkiewicz <tmisk@o2.pl>
2014-04-12 10:52:23 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gittest::Application.config.secret_key_base = '282700fe9b2452aa27249628d7a6e6c81bd95986965266c6b2f1ce885f4081e2f6929cbc4d61f3d632f7086ea53c15f6c5a5254261b2deab64be5c9730583ab5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gittest::Application.config.session_store :cookie_store, key: '_gittest_session'
